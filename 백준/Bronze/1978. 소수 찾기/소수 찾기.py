bf08b671ad4e654037e40333d825c8902477f64c
JMin0849 <121927205+JMin0849@users.noreply.github.com>
2024-10-21 21:38:14 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+data = list(map(int, input().split()))
+cnt = 0
+
+for i in data:
+    if i > 1:
+        for j in range(2, i+1):
+            if i % j == 0:
+                if i == j:
+                    cnt+=1
+                break
+            
+print(cnt)
