f3014ef9d323c1e45b97530c0b4049f8ef919fda
JMin0849 <121927205+JMin0849@users.noreply.github.com>
2024-10-22 21:41:31 +0900

    [Bronze III] Title: 대지, Time: 76 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,9 +1,11 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
+
 x_num, y_num = [], []
 
 for i in range(n):
-    x, y = map(int, input().split())
+    x, y = map(int, sys.stdin.readline().split())
     x_num.append(x)
     y_num.append(y)
     
-print((max(x_num)-min(x_num))*(max(y_num)-min(y_num)))  
+print((max(x_num)-min(x_num))*(max(y_num)-min(y_num)))
