9abc5c6f8ea3f726fbf2cad4c6ca0fc4bf463b21
JMin0849 <121927205+JMin0849@users.noreply.github.com>
2025-04-08 13:18:02 +0900

    [level 0] Title: 코드 처리하기, Time: 12.38 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,20 @@
+def solution(code):
+    answer = ''
+    mode = 0
+    
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] == '1':
+                mode = 1
+            elif i % 2 == 0:
+                answer += code[i]
+            
+        else:
+            if code[i] == '1':
+                mode = 0
+            elif i % 2 != 0:
+                answer += code[i]
+            
+    if not answer:
+        answer = 'EMPTY'
+    return answer
