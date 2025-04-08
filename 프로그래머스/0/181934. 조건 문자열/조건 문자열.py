e6379e5ab0008367bd5a7ae9e142acfd88c886b0
JMin0849 <121927205+JMin0849@users.noreply.github.com>
2025-04-08 12:40:22 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,12 @@
+def solution(ineq, eq, n, m):
+    
+    if eq == '!':
+        if ineq == '>':
+            return int(n>m)
+        else:
+            return int(m>n)
+    else:
+        if ineq == '>':
+            return int(n>=m)
+        else:
+            return int(m>=n)
