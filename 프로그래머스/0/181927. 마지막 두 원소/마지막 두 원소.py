1fdde9435f37464f91c25568fb0f37066b726958
JMin0849 <121927205+JMin0849@users.noreply.github.com>
2025-04-09 19:44:28 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,8 @@
+def solution(num_list):
+    answer = []
+    
+    if num_list[-1] > num_list[-2]:
+        answer = num_list + [num_list[-1] - num_list[-2]]
+    else: #num_list[-1] =< num_list[-2]:
+        answer = num_list + [num_list[-1]*2]
+    return answer
